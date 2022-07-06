070ecdddbf4e7c45385cf4d02fe035a01319482d
mtrehan <manveertrehan01@gmail.com>
2022-07-05 17:40:37 -0700

    mobile compatibilty improvements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import FetchNHLStats from './components/FetchNHLStats';
 
@@ -8,8 +7,8 @@ function App(props) {
   return (
     <div className="App">
       <div>
-        <a href="https://www.nhl.com/standings/2021/wildcard">
-          <img src={require('.//images/nhloldlogo.png')} className="img" alt="" />
+        <a href="https://www.nhl.com/standings/2021/wildcard" target="_blank">
+          <img src={require('.//images/nhloldlogo.png')} className="img" alt="NHL logo" />
         </a>
       </div>
       <FetchNHLStats />
--- src/components/FetchNHLStats.js
+++ src/components/FetchNHLStats.js
@@ -28,15 +28,12 @@ export default class FetchNHLStats extends React.Component {
             division: 3,
             loading: false,
         })
-        console.log(this.state)
     }
 
     handleClick(e, teamID, conf, teamDiv) {
 
         for (let i = 0; i < 8; i++) {
             if (this.state.league[teamDiv].teamRecords[i].team.id === teamID) {
-
-                console.log(e, teamID, conf, teamDiv)
 
                 this.setState({
                     team: this.state.league[teamDiv].teamRecords[i],
@@ -58,37 +55,28 @@ export default class FetchNHLStats extends React.Component {
                             <h1 className="header1">
                                 {this.state.team.team.name}
                             </h1>
-                            <div>
                                 Record: {this.state.team.leagueRecord.wins}-{this.state.team.leagueRecord.losses}-{this.state.team.leagueRecord.ot}
-                            </div>
-                            <div>
-                                Points: {this.state.team.points} | P%: {this.state.team.pointsPercentage}
-                            </div>
-                            <div>
+                                <br></br>
+                                Points: {this.state.team.points} | P%: {this.state.team.pointsPercentage.toFixed(3)}
+                                <br></br>
                                 Division Rank: {this.state.team.divisionRank} | Wilcard Rank: {this.state.team.wildCardRank}
-                            </div>
                             <hr>
                             </hr>
-                            <div>
                                 Right now, the {this.state.league[this.state.division].division.name} Division playoff bar is projected to be at: {Math.round(this.state.wildcard[this.state.division + 2].teamRecords[2].pointsPercentage * 164)} points
-                            </div>
-                            <div>
+                                <br></br>
                                 and the {this.state.league[this.state.division].conference.name} Conference wildcard playoff bar is projected to be at: {Math.round(this.state.wildcard[this.state.conference].teamRecords[1].pointsPercentage * 164)} points
-                            </div>
-                            <div className="blank">
-                                --
-                            </div>
+                            <br></br><br></br>
                             <div>
                                 In order to make the playoffs through a divisional spot, the {this.state.team.team.name} have to achieve a record of:
                                <div>
                                     {this.state.team.points >= Math.round(this.state.wildcard[this.state.division + 2].teamRecords[2].pointsPercentage * 164) ? (
-                                        <div className="good">
+                                        <div style={{color:'green'}}>
                                             Nothing! Even if they lose all their remaining games, they are already projected to make the playoffs in a divisional spot
                                         </div>
                                     ) : (
                                             <div>
                                                 {this.state.team.points + (2 * (82 - this.state.team.gamesPlayed)) < Math.round(this.state.wildcard[this.state.division + 2].teamRecords[2].pointsPercentage * 164) ? (
-                                                    <div className="bad">
+                                                    <div style={{color:'red'}}>
                                                         Even if they win all their remaining games, they are projected to miss the divisional spot
                                                     </div>
                                                 ) : (
@@ -103,18 +91,18 @@ export default class FetchNHLStats extends React.Component {
                                         )}
                                 </div>
                             </div>
-                            <div className="blank"> - </div>
+                            <br></br>
                             <div>
                                 In order to make the playoffs through a wildcard spot, the {this.state.team.team.name} have to achieve a record of:
                                <div>
                                     {this.state.team.points >= Math.round(this.state.wildcard[this.state.conference].teamRecords[1].pointsPercentage * 164) ? (
-                                        <div className="good">
+                                        <div style={{color:'green'}}>
                                             Nothing! Even if they lose all their remaining games, they are already projected to make the playoffs in a wildcard spot
                                         </div>
                                     ) : (
                                             <div>
                                                 {this.state.team.points + (2 * (82 - this.state.team.gamesPlayed)) < Math.round(this.state.wildcard[this.state.conference].teamRecords[1].pointsPercentage * 164) ? (
-                                                    <div className="bad">
+                                                    <div style={{color:'red'}}>
                                                         Even if they win all their remaining games, they are projected to miss the wildcard spot
                                                     </div>
                                                 ) : (
@@ -129,184 +117,120 @@ export default class FetchNHLStats extends React.Component {
                                         )}
                                 </div>
                             </div>
-                            <div className="blank"> -</div>
+                            <br></br>
                             <div>
                                 Select another team to see their current playoff status
                             </div>
-                            <div className="blank"> -</div>
+                            <br></br>
                             <div className="rowLine">
                                 <label className="divheader">
                                     Pacific Division:
-                                <div>
                                         <button onClick={e => this.handleClick(this, 24, 1, 3)}>
                                             Anaheim Ducks
                             </button>
-                                    </div>
-                                    <div>
                                         <button onClick={e => this.handleClick(this, 55, 1, 3)}>
                                             Seattle Kraken
                             </button>
-                                    </div>
-                                    <div>
                                         <button onClick={e => this.handleClick(this, 20, 1, 3)}>
                                             Calgary Flames
                             </button>
-                                    </div>
-                                    <div>
                                         <button onClick={e => this.handleClick(this, 22, 1, 3)}>
                                             Edmonton Oilers
                             </button>
-                                    </div>
-                                    <div>
                                         <button onClick={e => this.handleClick(this, 26, 1, 3)}>
                                             Los Angeles Kings
                             </button>
-                                    </div>
-                                    <div>
                                         <button onClick={e => this.handleClick(this, 28, 1, 3)}>
                                             San Jose Sharks
                             </button>
-                                    </div>
-                                    <div>
                                         <button onClick={e => this.handleClick(this, 23, 1, 3)}>
                                             Vancouver Canucks
                             </button>
-                                    </div>
-                                    <div>
                                         <button onClick={e => this.handleClick(this, 54, 1, 3)}>
                                             Vegas Golden Knights
                             </button>
-                                    </div>
                                 </label>
 
                                 <label className="divheader">
                                     Central Division:
-                                <div>
                                         <button onClick={e => this.handleClick(this, 53, 1, 2)}>
                                             Arizona Coyotes
                             </button>
-                                    </div>
-                                    <div>
                                         <button onClick={e => this.handleClick(this, 16, 1, 2)}>
                                             Chicago Blackhawks
                             </button>
-                                    </div>
-                                    <div>
                                         <button onClick={e => this.handleClick(this, 21, 1, 2)}>
                                             Colorado Avalanche
                             </button>
-                                    </div>
-                                    <div>
                                         <button onClick={e => this.handleClick(this, 25, 1, 2)}>
                                             Dallas Stars
                             </button>
-                                    </div>
-                                    <div>
                                         <button onClick={e => this.handleClick(this, 30, 1, 2)}>
                                             Minnesota Wild
                             </button>
-                                    </div>
-                                    <div>
                                         <button onClick={e => this.handleClick(this, 18, 1, 2)}>
                                             Nashville Predators
                             </button>
-                                    </div>
-                                    <div>
                                         <button onClick={e => this.handleClick(this, 19, 1, 2)}>
                                             St. Louis Blues
                             </button>
-                                    </div>
-                                    <div>
                                         <button onClick={e => this.handleClick(this, 52, 1, 2)}>
                                             Winnipeg Jets
                             </button>
-                                    </div>
                                 </label>
                                 <label className="divheader">
                                     Atlantic Division:
-                                <div>
                                         <button onClick={e => this.handleClick(this, 6, 1, 1)}>
                                             Boston Bruins
                             </button>
-                                    </div>
-                                    <div>
                                         <button onClick={e => this.handleClick(this, 7, 1, 1)}>
                                             Buffalo Sabres
                             </button>
-                                    </div>
-                                    <div>
                                         <button onClick={e => this.handleClick(this, 17, 1, 1)}>
                                             Detroit Red Wings
                             </button>
-                                    </div>
-                                    <div>
                                         <button onClick={e => this.handleClick(this, 13, 1, 1)}>
                                             Florida Panthers
                             </button>
-                                    </div>
-                                    <div>
                                         <button onClick={e => this.handleClick(this, 8, 1, 1)}>
                                             Montréal Canadiens
                             </button>
-                                    </div>
-                                    <div>
                                         <button onClick={e => this.handleClick(this, 9, 1, 1)}>
                                             Ottawa Senators
                             </button>
-                                    </div>
-                                    <div>
                                         <button onClick={e => this.handleClick(this, 14, 1, 1)}>
                                             Tampa Bay Lightning
                             </button>
-                                    </div>
-                                    <div>
                                         <button onClick={e => this.handleClick(this, 10, 1, 1)}>
                                             Toronto Maple Leafs
                             </button>
-                                    </div>
                                 </label>
                                 <label className="divheader">
                                     Metropolitan Division:
-                                <div>
                                         <button onClick={e => this.handleClick(this, 12, 1, 0)}>
                                             Carolina Hurricanes
                             </button>
-                                    </div>
-                                    <div>
                                         <button onClick={e => this.handleClick(this, 29, 1, 0)}>
                                             Columbus Blue Jackets
                             </button>
-                                    </div>
-                                    <div>
                                         <button onClick={e => this.handleClick(this, 1, 1, 0)}>
                                             New Jersey Devils
                             </button>
-                                    </div>
-                                    <div>
                                         <button onClick={e => this.handleClick(this, 2, 1, 0)}>
                                             New York Islanders
                             </button>
-                                    </div>
-                                    <div>
                                         <button onClick={e => this.handleClick(this, 3, 1, 0)}>
                                             New York Rangers
                             </button>
-                                    </div>
-                                    <div>
                                         <button onClick={e => this.handleClick(this, 4, 1, 0)}>
                                             Philadelphia Flyers
                             </button>
-                                    </div>
-                                    <div>
                                         <button onClick={e => this.handleClick(this, 5, 1, 0)}>
                                             Pittsburgh Penguins
                             </button>
-                                    </div>
-                                    <div>
                                         <button onClick={e => this.handleClick(this, 15, 1, 0)}>
                                             Washington Capitals
                             </button>
-                                    </div>
                                 </label>
                             </div>
                         </div>
